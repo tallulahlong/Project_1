0eedaf11ae63bbe153d2efcd7648cb3ab927badd
ganjioppa <Timkim@Tims-MacBook.local>
2018-10-13 13:09:04 -0700

    saving

--- map.js
+++ map.js
@@ -4,6 +4,10 @@ var placesAPI = "AIzaSyADGBWsEdFbuCg0xzSfPVrbm1mihab7ro4";
 var myKeyword = "Japanese";
 var myLocation;
 var myfields = "photos,formatted_address,name,rating, opening_hours"
+var price_level1 = [];
+var price_level2 = [];
+var price_level3 = [];
+var price_level4 = [];
 
 function initAutocomplete() {
   geocoder = new google.maps.Geocoder();
@@ -18,10 +22,10 @@ function codeAddress(address) {
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == 'OK') {
 
-        alert(results[0].geometry.location);
+        console.log(results[0].geometry.location);
 
       } else {
-        alert('Geocode was not successful for the following reason: ' + status);
+        console.log('Geocode was not successful for the following reason: ' + status);
       }
     });
   }
@@ -50,16 +54,63 @@ function fillInAddress() {
   
   function secondRequest(coordinates){
     $.ajax({
-      url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+coordinates+"&radius=1500&type=restaurant&keyword="+myKeyword+"&fields="+myfields+"&key="+placesAPI,
+      url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+coordinates+"&rankby=distance&type=restaurant&keyword="+myKeyword+"&fields="+myfields+"&key="+placesAPI,
       method: "GET"
       }).then(function(response) {
       console.log(coordinates);
       console.log(response);
+      console.log(response.results.length);
+      console.log(response.results[0].price_level);
+      console.log(response.results[0]);
+      for (var i = 0; i < response.results.length; i++){
+        if(response.results[i].price_level === 1){
+          price_level1.push(response.results[i]);
+        }
+        else if(response.results[i].price_level === 2){
+          price_level2.push(response.results[i]);
+        }
+        else if(response.results[i].price_level === 3){
+          price_level3.push(response.results[i]);
+        }
+        else{
+          price_level4.push(response.results[i]);
+        }
+      }
+      
+      console.log(price_level1);
+      console.log(price_level2);
+      console.log(price_level3);
+      console.log(price_level4);
+      console.log(price_level1[0].rating);
+      findHighestRating(price_level1);
+      findHighestRating(price_level2);
+      findHighestRating(price_level3);
+      findHighestRating(price_level4);
+
+      
     });
   }
 
+  function findHighestRating(array) {
+    var max = 0;
+    var maxObject;
+    for(var i = 0; i < array.length; i++){
+      if(array[i].rating > max){
+        max = array[i].rating;
+        maxObject = array[i];
+      }
+    }
+    console.log(max);
+    console.log(maxObject);
+    return max, maxObject;
+    
+  }
 
 }
+
+
+
+
 
  
 
